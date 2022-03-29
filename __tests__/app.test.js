8fa4935cab6d2a6df8e13d226645841ea44c50cf
Mark Voltaire <markjvoltaire@gmail.com>
2022-03-29 17:58:27 -0400

    added info inside of test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const hats
 
 describe('top-secrects routes', () => {
   beforeEach(() => {
@@ -12,7 +13,12 @@ describe('top-secrects routes', () => {
     pool.end();
   });
 
-  it('a user by sending a post route', async () => {
-    
+  it('signs a user up by sending a post route', async () => {
+    const res = await request(app)
+    .post('/api/v1/users')
+    .send({username: 'mark', password: 'helloworld'})
+
+
+    expect(res.body).toEqual({id: expect.any(String), username: 'mark'})
   });
 });
