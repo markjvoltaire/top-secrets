ec83025459a9a0f84853d9ddb9db5164c259520f
Mark Voltaire <markjvoltaire@gmail.com>
2022-03-29 19:36:58 -0400

    test without middleware passed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const users = require('../lib/controllers/users');
 
 describe('top-secrects routes', () => {
   beforeEach(() => {
@@ -14,9 +15,15 @@ describe('top-secrects routes', () => {
 
   it('signs a user up by sending a post route', async () => {
     const res = await request(app)
-      .post('/api/v1/users/signup')
+      .post('/api/v1/users')
       .send({ username: 'mark', password: 'helloworld' });
 
-    expect(res.body).toEqual({ id: expect.any(String), username: 'mark' });
+    console.log('res', res.body);
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      username: 'mark',
+      password: 'helloworld',
+    });
   });
 });
